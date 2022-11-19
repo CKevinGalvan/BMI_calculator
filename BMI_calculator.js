fc996c2409948482ad4664c1691df20343012cf4
Carl Kevin Galvan <99018607+CKevinGalvan@users.noreply.github.com>
2022-11-18 16:53:09 -0800

    Update BMI_calculator.js

--- BMI_calculator.js
+++ BMI_calculator.js
@@ -1,5 +1,27 @@
-function bmiCalculator(weight, height)
+function bmiCalculator (weight, height) 
 {
-    return  Math.round(weight)/(height*height);
+    var interpretation = "";
+    var bmi = Math.floor((weight)/(height*height));
+    console.log(bmi);
+    if(bmi<18.5)
+    {
+        interpretation = "Your BMI is "+bmi+", so you are underweight.";
+    }
+    else if(bmi>18.5 && bmi<=24.9)
+    {
+        interpretation = "Your BMI is "+bmi+", so you have a normal weight";
+    }
+    else if(bmi>24.9 && bmi<=29.9)
+    {
+        interpretation = "Your BMI is "+bmi+", so you are overweight.";
+    }
+    else if(bmi>30 && bmi<=34.9)
+    {
+        interpretation = "Your BMI is "+bmi+", so you are obese.";
+    }
+    else if(bmi>35)
+    {
+        interpretation = "Your BMI is "+bmi+", so you are extremely obese.";
+    }
+    return interpretation;
 }
-var bmi = bmiCalculator(65, 1.8); 
